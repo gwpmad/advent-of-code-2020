390a4354006fd7872c462523ae7f5eb5e73aaea7
gwpmad <gmaddocks1@gmail.com>
2020-12-18 12:06:48 +0000

    11/1

---
+++ 11/1.js
@@ -0,0 +1,91 @@
+const fs = require('fs');
+const seats = fs.readFileSync(__dirname + '/input', 'utf8');
+
+function getSlotName([rowIdx, colIdx]) {
+  return `${rowIdx}-${colIdx}`;
+}
+
+function getSurroundingSeats(rowIdx, colIdx, numberOfColumns, numberOfRows) {
+  const rowAbove = rowIdx - 1, rowBelow = rowIdx + 1, leftCol = colIdx - 1, rightCol = colIdx + 1;
+  return [
+    [rowAbove, leftCol],
+    [rowAbove, colIdx],
+    [rowAbove, rightCol],
+    [rowIdx, leftCol],
+    [rowIdx, rightCol],
+    [rowBelow, leftCol],
+    [rowBelow, colIdx],
+    [rowBelow, rightCol],
+  ].filter(([_rowIdx, _colIdx]) =>
+    _colIdx > -1 && _colIdx < numberOfColumns
+    && _rowIdx > -1 && _rowIdx < numberOfRows
+  ).map(getSlotName);
+}
+
+function getInitialSeatState(input) {
+  const rows = input.split('\n');
+  const state = { graph: new Map(), occupiedSeats: 0, visitQueue: [], visitedSeats: new Set() };
+  for (let rowIdx = 0; rowIdx < rows.length; rowIdx++) {
+    const row = rows[rowIdx];
+    for (let colIdx = 0; colIdx < row.length; colIdx++) {
+      const type = row[colIdx] === 'L' ? 'empty' : 'floor';
+      state.graph[getSlotName([rowIdx, colIdx])] = {
+        type,
+        newType: type,
+        surroundingSeats: getSurroundingSeats(rowIdx, colIdx, row.length, rows.length),
+      };
+    }
+  }
+  return state;
+}
+
+function applyRules(seat, seatState) {
+  if (seat.type === 'floor') return;
+  const adjacentOccupiedSeats = seat.surroundingSeats.filter(
+    name => seatState.graph[name].type === 'occupied').length;
+  if (seat.type === 'empty' && !adjacentOccupiedSeats) {
+    seat.newType = 'occupied';
+    seatState.occupiedSeats++;
+  }
+  if (seat.type === 'occupied' && adjacentOccupiedSeats > 3) {
+    seat.newType = 'empty';
+    seatState.occupiedSeats--;
+  }
+}
+
+function traverseSeats(startSeat, seatState) {
+  const { visitQueue, visitedSeats, graph } = seatState;
+  visitQueue.push(startSeat);
+  visitedSeats.add(startSeat);
+
+  while (visitQueue.length) {
+    const currentSeat = graph[visitQueue.shift()];
+    applyRules(currentSeat, seatState);
+    
+    for (let seat of currentSeat.surroundingSeats) {
+      if (visitedSeats.has(seat)) continue;
+      visitQueue.push(seat);
+      visitedSeats.add(seat);
+    }
+  }
+}
+
+function prepareSeatState(seatState) {
+  for (const seat of Object.values(seatState.graph)) {
+    seat.type = seat.newType;
+  }
+  seatState.visitedSeats.clear();
+}
+
+function findNumberOfChangesBeforeStatic(input) {
+  const seatState = getInitialSeatState(input);
+  let occupiedSeatsBefore = seatState.occupiedSeats;
+  do {
+    prepareSeatState(seatState)
+    occupiedSeatsBefore = seatState.occupiedSeats;
+    traverseSeats('0-0', seatState);
+  } while (seatState.occupiedSeats !== occupiedSeatsBefore)
+  return seatState.occupiedSeats;
+}
+
+console.log(findNumberOfChangesBeforeStatic(seats));
